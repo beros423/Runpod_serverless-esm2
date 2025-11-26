861d31070d18f28c97f7894a7dcbd1874f4ed5cf
Gyeongmin Park <123447806+beros423@users.noreply.github.com>
2025-11-26 10:44:52 +0900

    Implement handler for generating embeddings

---
+++ rp_handler.py
@@ -0,0 +1,31 @@
+import runpod
+
+import sys
+import os
+from pathlib import Path
+from main.esm_embedding import embed_sequences
+
+
+def handler(event):    
+    # base_path = r"D:\Git_Clone\GeneExp"
+    # sys.path.append(str(Path(base_path)))
+    print(f"Worker Start")
+
+    translations = event['input'].get('translations')
+
+    if not translations:
+        raise(ValueError(f"No translations provided: {event['input']}"))
+
+    embeddings = embed_sequences(translations)
+    #########################################
+        
+
+    return {
+        "status": "success",
+        "message": f"Generated embeddings for {len(embeddings)} sequences.",
+        "embeddings": embeddings
+    }
+
+
+if __name__ == '__main__':
+    runpod.serverless.start({'handler': handler})
